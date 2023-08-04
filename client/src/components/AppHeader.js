99b729dd9c5fd4fb60ea417d06da479a6885f255
Ms Eko <jomaduagwu@gmail.com>
2023-08-03 21:34:20 -0500

    updated header

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,69 +1,30 @@
-import React from 'react'
-import { Layout, Menu } from 'antd';
+import React from 'react';
+import { DatePicker, Input, Button } from 'antd';
 import { Link } from 'react-router-dom';
+import './header.css';
+import WanderJoLogo from '../assets/updatedWJ-logo.png';
 
 
-// const nav_links = [
-//     {
-//         path: '/home',
-//         display: 'Home'
-//     },
-//     {
-//         path: '/about',
-//         display: 'About'
-//     },
-//     {
-//         path: '/travel',
-//         display: 'Travel'
-//     },
-    
-// ]
-const {  Header } = Layout;
+
 
 const AppHeader = () => {
   return (
-    <Header style={{ background: '#fff', padding: '0 20px', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-      <div>
-        <span style={{ marginRight: '20px', fontSize: '1.5rem', fontWeight: 'bold' }}>
-          WanderJo Travel Planner
-        </span>
+    // <div className="ant-menu ant-menu-light ant-menu-horizontal header-container">
+    <div className="header-container">
+      <div className="logo">
+      <Link to="/" className="ant-menu-item ant-menu-item-only-child">
+        <b>
+          <img src={WanderJoLogo} alt="WanderJo Travel Planner" style={{ marginRight: '10px', opacity: 0.8 }} />
+        </b>
+      </Link>
       </div>
-      <Menu theme= "light" mode="horizontal" defaultSelectedKeys={['home']}>
-        <Menu.Item key="home" as={Link} to="/">
-          Home
-        </Menu.Item>
-        <Menu.Item key="login" as={Link} to="/login">
-          Login
-        </Menu.Item>
-      </Menu>
-    </Header>
-    // <><header className='header'>
-    //       <Container>
-    //           <Row justify='space-between' align='middle'>
-    //               {/* Logo */}
-    //               <div className='logo'>
-    //                   <Link to='/'>
-    //                       <img
-    //                           src='../assets/WanderJoLogo.png'
-    //                           alt='WanderJo Travel Planner' />
-    //                   </Link>
-    //               </div>
+      <div className="search-container">
+        <Input size="large" placeholder="Type your destination" className="search-input" />
+        <DatePicker className="date-picker" />
+        <Button type="primary" className="search-button">Search</Button>
+      </div>
+    </div>
+  );
+};
 
-    //               {/* Navigation Links */}
-    //               <Menu mode='horizontal' theme='dark' style={{ lineHeight: '64px' }}>
-    //                   {nav_links.map((link) => (
-    //                       <Menu.Item key={link.path}>
-    //                           <NavLink to={link.path}>{link.display}</NavLink>
-    //                       </Menu.Item>
-    //                   ))}
-    //               </Menu>
-    //           </Row>
-    //       </Container>
-    //   </header><div>
-    //           Header
-    //       </div></>
-  )
-}
-
-
-export default AppHeader
+export default AppHeader;
